09ebe8f138b97d52954e1e456b58d8669ba58bf9
Bosco Andrade  Bravo <boscoand@hotmail.com>
2017-06-25 09:35:13 -0500

    Corrección: Liberación de memoria en Lista_SacarTodos.c

--- src/Lista_SacarTodos.c
+++ src/Lista_SacarTodos.c
@@ -5,6 +5,11 @@
 void Lista_SacarTodos(ListaEnlazada *lista){
 
   ElementoLista *ancla = &(lista->ancla);
+  ElementoLista *elem;
+
+  for (elem = Lista_Primero(lista); elem != NULL; elem = Lista_Siguiente(lista, elem)) {
+    free(elem);
+  }
 
   ancla->anterior = NULL;
   ancla->siguiente = NULL;
